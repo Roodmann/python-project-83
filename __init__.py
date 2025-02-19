7106dcce762b6ed7a34e4fd1ec9260df7b011c92
Dima <dima_rood@inbox.ru>
2025-02-19 22:21:01 +0300

    full page pack

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from page_analyzer.app import app
+
+
+
+__all__ = ['app']
+
+
+
---
+++ page_analyzer/app.py
@@ -0,0 +1,15 @@
+
+from flask import Flask
+import os
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.run()
+
+@app.route('/')
+def index():
+    return 'Привет, это страница анализатора!'
